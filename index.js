1eca72f13f11dff1cb5b082e2f98bad903109ce9
root <root@fearpi.com>
2018-08-17 15:50:21 +0000

    log with timestamps; do not post to slack on first fetch; enqueue next fetch on fetch error

--- index.js
+++ index.js
@@ -6,22 +6,26 @@ var cheerio = require('cheerio');
 var POLL_INTERVAL = 30 * 1000;
 var lastActivity;
 
+function log(s) {
+  console.log((new Date()).toISOString() + ": " + s);
+}
+
 function postActivity(activity){
-  console.log('posting to slack...');
+  log('posting to slack...');
   superagent
     .post('https://hooks.slack.com/services/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')
     .type('json')
     .send({text: activity})
     .then((res) => {
-      console.log("Posted to slack: " + res.status);
+      log("Posted to slack: " + res.status);
     })
     .catch((e) => {
-      console.error("ERROR posting to slack: " + e.message);
+      log("ERROR posting to slack: " + e.message);
     });
 }
 
 function doIt(){
-  console.log('fetching activity...');
+  log('fetching activity...');
   http.get('http://games.espn.com/ffl/leagueoffice?leagueId=420354&seasonId=2018', (res) => {
     var body = '';
     res.on('data', (chk) => body += chk);
@@ -30,15 +34,18 @@ function doIt(){
       var activity = $("ul#lo-recent-activity-list>li .recent-activity-fulldesc")
         .toArray()
         .map((e) => $(e).text()); // newest first
-      console.log('fetched activity');
+      log('fetched activity');
       if (activity[0] != lastActivity) {
+        if (lastActivity) {
+          postActivity(activity[0]);
+        }
         lastActivity = activity[0];
-        postActivity(lastActivity);
       }
       setTimeout(doIt, POLL_INTERVAL);
     });
   }).on('error', (e) => {
-    console.error('error: ' + e.message);
+    log('error: ' + e.message);
+    setTimeout(doIt, POLL_INTERVAL);
   });
 }
 
